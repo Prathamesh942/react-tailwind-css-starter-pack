7a93114b45f57a86445516164e498cc6b2a35dfd
prathamesh <prathameshsk94@gmail.com>
2025-03-07 14:34:06 +0530

    dynamic bg

--- src/HarmonicaPortfolio.jsx
+++ src/HarmonicaPortfolio.jsx
@@ -12,12 +12,21 @@ export default function HarmonicaPortfolio() {
     { title: "Take me home, country roads", file: "/recordings/tmhcr.mp3" },
   ];
 
+  const bgs = [
+    "https://i.pinimg.com/736x/30/0d/0d/300d0d5e5fe20a96a25a06bab83ee057.jpg",
+    "https://i.pinimg.com/736x/8f/48/de/8f48def5bfe1aa3c31e32cba5d391847.jpg",
+    "https://i.pinimg.com/736x/0d/e9/a5/0de9a51fc3a25d830da608af9b038878.jpg",
+    "https://i.pinimg.com/736x/86/32/4d/86324d18a5635017df66df6a8d877bb0.jpg",
+    "https://i.pinimg.com/736x/bc/a4/80/bca480a3d79e52007706a45126d54590.jpg",
+    "https://i.pinimg.com/736x/d0/17/70/d017709e3521c04bdbc4ba1468b8a9c0.jpg",
+  ];
+
   return (
     <div className="relative min-h-screen flex flex-col justify-center text-center space-y-6 px-10 text-white">
       {/* Background Image */}
       <div className="fixed top-0 left-0 w-full h-full -z-10">
         <img
-          src="https://i.pinimg.com/736x/99/68/e6/9968e6f1e8894e6dd69b3c4823185429.jpg"
+          src={bgs[currentTrack]}
           alt="Background"
           className="w-full h-full object-cover"
         />
