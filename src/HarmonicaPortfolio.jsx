7614584662a3d498a88ffc245d476c1a66694ff1
prathamesh <prathameshsk94@gmail.com>
2025-03-09 21:00:15 +0530

    new songs

--- src/HarmonicaPortfolio.jsx
+++ src/HarmonicaPortfolio.jsx
@@ -10,6 +10,8 @@ export default function HarmonicaPortfolio() {
     { title: "Kal Ho Na Ho", file: "/recordings/khnh.mp3" },
     { title: "My Heart Will Go On", file: "/recordings/tn.mp3" },
     { title: "Take Me Home, Country Roads", file: "/recordings/tmhcr.mp3" },
+    { title: "Fly me to the moon", file: "/recordings/fmttm.mp3" },
+    { title: "Blowin' in the wind", file: "/recordings/bitw.mp3" },
   ];
 
   const bgs = [
@@ -19,13 +21,15 @@ export default function HarmonicaPortfolio() {
     "https://i.pinimg.com/736x/86/32/4d/86324d18a5635017df66df6a8d877bb0.jpg",
     "https://i.pinimg.com/736x/bc/a4/80/bca480a3d79e52007706a45126d54590.jpg",
     "https://i.pinimg.com/736x/d0/17/70/d017709e3521c04bdbc4ba1468b8a9c0.jpg",
+    "https://i.pinimg.com/736x/7e/48/64/7e48642733c77d4d97ce127d6b7367bb.jpg",
+    "https://i.pinimg.com/736x/71/24/f2/7124f2268e6d7545dcce2e71a1cee8c5.jpg",
   ];
 
   return (
     <div className="relative min-h-screen flex flex-col justify-center text-center space-y-6 px-10 text-white">
       {/* Background Image */}
-      <div className="fixed top-0 left-0 w-full h-full -z-10">
-        <img
+      <div className="fixed top-0 left-0 w-full h-full -z-10 bg-[#131313]">
+        {/* <img
           src={
             currentTrack
               ? bgs[
@@ -37,13 +41,31 @@ export default function HarmonicaPortfolio() {
           }
           alt="Background"
           className="w-full h-full object-cover"
-        />
+        /> */}
       </div>
 
       {/* Audio Player */}
       {currentTrack && (
-        <div className="mt-4 bg-opacity-70 p-4 rounded-lg">
-          <audio controls autoPlay src={currentTrack} className="w-full mt-2" />
+        <div className=" p-4 rounded-lg flex flex-col justify-center items-center sticky top-6 bg-[#131313] z-20">
+          <img
+            src={
+              currentTrack
+                ? bgs[
+                    recordings.indexOf(
+                      recordings.find((rec) => rec.file === currentTrack)
+                    )
+                  ]
+                : "https://i.pinimg.com/736x/99/68/e6/9968e6f1e8894e6dd69b3c4823185429.jpg"
+            }
+            className=" aspect-square object-cover rounded-xl"
+            alt=""
+          />
+          <audio
+            controls
+            autoPlay
+            src={currentTrack}
+            className="w-full mt-2 "
+          />
         </div>
       )}
 
@@ -58,15 +80,12 @@ export default function HarmonicaPortfolio() {
           {recordings.map((rec, index) => (
             <div
               key={index}
-              className="flex items-center justify-between bg-[#3f332a56] p-3 rounded-lg"
+              className={`flex items-center justify-between rounded-lg overflow-scrol p-2 ${
+                currentTrack == rec.file ? "bg-[#474d9642]" : ""
+              } `}
+              onClick={() => setCurrentTrack(rec.file)}
             >
               <span className="text-lg">{rec.title}</span>
-              <button
-                onClick={() => setCurrentTrack(rec.file)}
-                className="px-4 py-2 bg-white text-black rounded-lg"
-              >
-                {currentTrack === rec.file ? "Pause" : "Play"}
-              </button>
             </div>
           ))}
         </div>
