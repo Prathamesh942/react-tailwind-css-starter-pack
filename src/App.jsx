4173043ca3b99a8f297654346e967fe35ef64fb0
prathamesh <prathameshsk94@gmail.com>
2025-03-15 12:26:56 +0530

    p

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import HarmonicaPortfolio from "./HarmonicaPortfolio";
 
 function App() {
   return (
-    <div className=" text-xl">
+    <div className=" text-xl p-5">
       hii titir I already miss you so much (I couldnt control the urge to write
       you back so i removed you i cant find you anymore on slowly) 😿 I assume
       you miss me too if you have opened this website intentionally, if you do
