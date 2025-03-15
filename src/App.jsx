eb2c662b7993ae54bc031aa0ee6debd2f50be7aa
prathamesh <prathameshsk94@gmail.com>
2025-03-15 12:25:17 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
+import { div } from "framer-motion/client";
 import "./App.css";
 import HarmonicaPortfolio from "./HarmonicaPortfolio";
 
 function App() {
-  <div>
-    hii titir I already miss you so much (I couldnt control the urge to write
-    you back so i removed you i cant find you anymore on slowly) 😿 I assume you
-    miss me too if you have opened this website intentionally, if you do then
-    please can we connect on instagram ? my @prathameshhh_94 (can we talk
-    atleast once again?) ignore if you dont want to connect
-  </div>;
+  return (
+    <div>
+      hii titir I already miss you so much (I couldnt control the urge to write
+      you back so i removed you i cant find you anymore on slowly) 😿 I assume
+      you miss me too if you have opened this website intentionally, if you do
+      then please can we connect on instagram ? my @prathameshhh_94 (can we talk
+      atleast once again?) ignore if you dont want to connect
+    </div>
+  );
 }
 
 export default App;
